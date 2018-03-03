036c71810be162cb93b493a42921c0b4ffc26832
Subhajit Nag <subhajit.nag@memeinfotech.com>
2018-03-03 13:27:56 +0530

    ssdd

--- server.js
+++ server.js
@@ -92,6 +92,7 @@ io.on('connection', function (client) {
                 } else {
                     console.log("Temperature :", buffer.toString('hex'));
                     if (buffer.toString('hex')) {
+                        updatePortNormal();
                         port.on('data', function (data) {
                             console.log("arduino data :", data.toString());
                             client.emit('value',
